9ba25197cdfa7d821570043f16f9231aafbeaa7b
AndrewChebli <andrew.chebli@outlook.com>
2025-01-28 20:17:23 -0500

    Add query requirement

---
+++ sql/query5.sql
@@ -0,0 +1,7 @@
+/*
+For a given family member, get details of all club members associated with him/her.
+Information includes club membership number, first-name, last-name, date of birth,
+Social Security Number, Medicare card number, telephone number, address, city,
+province, postal code, relationship with the family member, and status (active or
+inactive). 
+*/
